3b03a6614dd5fb0ffe818eaade83533ece03605a
vahid koohjani <vk1604@yahoo.com>
2023-11-21 12:18:43 +0100

    refac: s3

--- infra/modules/s3-website/main.tf
+++ infra/modules/s3-website/main.tf
@@ -28,3 +28,27 @@ resource "aws_s3_bucket_public_access_block" "this" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "arn:aws:s3:::${aws_s3_bucket.this.id}/*",
+        "arn:aws:s3:::${aws_s3_bucket.this.id}"
+      ]
+    }
+  ]
+}
+POLICY
+}
