e38f410c892dba485b8f6696cdac5acee2828f80
vahid koohjani <vk1604@yahoo.com>
2023-11-22 11:54:59 +0100

    12

--- infra/modules/s3-website/main.tf
+++ infra/modules/s3-website/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "this" {
-  bucket = "my-website-cicd-github2023" #var.bucket_name
+  bucket = var.bucket_name
 
   # Allow deletion of non-empty bucket
   force_destroy = true
